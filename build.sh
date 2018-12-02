8b7949de5db0061e85b1d1409dcb8a640debc0bc
wbyin <redondo.198679@hotmail.com>
2018-12-02 16:15:18 +0800

    aaa

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ fi
 
 rm -rf output
 mkdir output
-app="scrawler-1.0-SNAPSHOT.jar"
+app="scrawler-0.0.1-SNAPSHOT.jar"
 mv target/${app} output/  #拷贝目标war包或者jar包等 至output目录下
 
-nohup $JAVA_HOME/bin/java -jar -server -Dspring.profiles.active=prod -Xms1G -Xmx1G -XX:+DisableExplicitGC -XX:+UseG1GC -XX:MaxGCPauseMillis=500 ${app} > ~/gitlab/scrawler/nohup.log 2>&1 &
+nohup $JAVA_HOME/bin/java -jar -server -Dspring.profiles.active=prod -Xms1G -Xmx1G -XX:+DisableExplicitGC -XX:+UseG1GC -XX:MaxGCPauseMillis=500 ~/gitlab/scrawler/output/${app} > ~/gitlab/scrawler/nohup.log 2>&1 &
 echo "App started"
